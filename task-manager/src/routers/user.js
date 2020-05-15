17425a7268bbd68b25c0ac0e56138df6e089aa8f
shivaprasadgit <shivaprasad.ds@outlook.com>
2020-05-15 12:03:25 +0530

    comented

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -14,7 +14,7 @@ router.post('/users', async (req, res) => {
         res.status(400).send(e)
     }
 })
-
+///this is added//
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
